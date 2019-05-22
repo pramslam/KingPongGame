a45b8df12706fb693faf0f5b0e17649b78c0d048
David Lo <pramslam@gmail.com>
2019-05-21 23:13:19 -0700

    Timer Text
    Fixed timer text, numbers under 10 now display with a 0 before it.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,13 +10,13 @@ namespace DLO   {
         public int startTime = 180;         // In seconds
         public Text timerText;              // UI text object
 
-        private bool isRunning = false;     // For pausing
+        private bool isPaused;              // For pausing
         private int timeLeft;               // Seconds left overall
         
         // Start is called before the first frame update
         void Start()
         {
-            isRunning = true;
+            isPaused = false;
             timeLeft = startTime;
             StartCoroutine("LoseTime");
             Time.timeScale = 1;             // Ensure the timescale is right
@@ -32,7 +32,7 @@ namespace DLO   {
         // Coroutine
         IEnumerator LoseTime()
         {
-            while (isRunning)
+            while (!isPaused)
             {
                 yield return new WaitForSeconds(1);
                 timeLeft--;
@@ -41,17 +41,42 @@ namespace DLO   {
 
         string ConvertSteps2Time(float steps)
         {
-            string seconds = ((int)steps % 60).ToString("F0");
-            string minutes = (((int)steps / 60) % 60).ToString("F0");
+            string minutes, seconds;
+
+            int secs = ((int)steps % 60);
+
+            // Format text for numbers smaller than 10
+            if (secs < 10) { seconds = "0" + secs.ToString("F0"); }
+            else           { seconds = secs.ToString("F0"); }
+            
+            minutes = (((int)steps / 60) % 60).ToString("F0");
 
             return minutes + " : " + seconds;
         }
 
         // Public Functions
         #region
-        public void PauseTimer() { isRunning = !isRunning; }
+        public void PauseTimer()
+        {
+            if (isPaused)
+            {
+                isPaused = false;
+                StartCoroutine("LoseTime");
+            }
+            else if (!isPaused)
+            {
+                isPaused = true;
+                StopCoroutine("LoseTime");
+            }
+        }
 
-        public void ResetTimer() { timeLeft = startTime; }
+        public void ResetTimer()
+        {
+            timeLeft = startTime;
+            isPaused = false;
+            StopCoroutine("LoseTime");
+            StartCoroutine("LoseTime");
+        }
 
         public int GetTimeLeft() { return timeLeft; }
         #endregion
